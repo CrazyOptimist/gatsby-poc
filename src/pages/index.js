387b5583161f87509114cc80abefb01b91b20337
CrazyOptimist <crazyoptimist@mail.com>
2022-01-05 13:05:57 -0600

    add posts word to the index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ const IndexPage = ({ data }) => (
     <Seo title="Home" />
     <div>
       <h1>DOCKERLEAD's Thoughts</h1>
-      <h4>{data.allMarkdownRemark.totalCount}</h4>
+      <h4>{data.allMarkdownRemark.totalCount} Posts in Total</h4>
       {data.allMarkdownRemark.edges.map(({ node }) => (
         <div key={node.id}>
           <BlogLink to={node.fields.slug}>
