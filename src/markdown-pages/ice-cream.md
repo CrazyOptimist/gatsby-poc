c48dd77b5f24be7aaad59f9c0f3e44931ee731ce
CrazyOptimist <crazyoptimist@mail.com>
2022-01-05 11:23:05 -0600

    use markdown data using graphql wow - gatsby is so smart I say

---
+++ src/markdown-pages/ice-cream.md
@@ -0,0 +1,17 @@
+---
+title: "Why I Love Ice Cream"
+date: "2022-01-05"
+---
+
+### Test Blog Post About Ice Cream
+
+I scream, you scream, we all scream, because there is no more ice cream.
+
+[link example](https://crazyoptimist.net)
+
+> quote example
+> second line of the quote
+
+```bash
+ls -al
+```
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,42 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
-
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
-    </p>
+    <div>
+      <h1>DOCKERLEAD's Thoughts</h1>
+      <h4>{data.allMarkdownRemark.totalCount}</h4>
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <div key={node.id}>
+          <span>
+            {node.frontmatter.title} - {node.frontmatter.date}
+          </span>
+          <p>{node.excerpt}</p>
+        </div>
+      ))}
+    </div>
   </Layout>
 )
 
 export default IndexPage
+
+export const query = graphql`
+  {
+    allMarkdownRemark {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date
+            description
+          }
+          excerpt
+        }
+      }
+      totalCount
+    }
+  }
+`
