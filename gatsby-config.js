d6763161d578082aa003b3d56a5bc1eef3d9d95e
CrazyOptimist <crazyoptimist@mail.com>
2022-01-05 03:04:01 -0600

    try to modify metadata and see how it works with graphql static querying

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `DOCKERLEAD's Blog`,
+    description: `Make today another colorful one, try hard and enjoy`,
+    author: `crazyoptimist`,
+    siteUrl: `https://blog.crazyoptimist.net/`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
